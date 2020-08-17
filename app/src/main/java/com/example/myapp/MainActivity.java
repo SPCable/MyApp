f410d60ef1395baade16ab4e18cc0f15f3f94107
Alars99 <hoanglong291099@gmail.com>
2020-08-17 14:13:55 +0700

    update_17/8

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -31,15 +31,15 @@ public class MainActivity extends AppCompatActivity implements FoodAdapter.OnFoo
         setContentView(R.layout.activity_main);
 
         List<Food_sp> food_sps = new ArrayList<>();
-        food_sps.add(new Food_sp("Thịt bò Mỹ-Canada-Nga", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Thịt bò Úc", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Thịt Cừu", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Thịt Trâu", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Thịt Heo", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Thịt Gà", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Thịt Dê", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Hải Sản", R.drawable.beefsteak));
-        food_sps.add(new Food_sp("Sản phẩm khác", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("1Thịt bò Mỹ-Canada-Nga", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("2Thịt bò Úc", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("3Thịt Cừu", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("4Thịt Trâu", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("5Thịt Heo", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("6Thịt Gà", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("7Thịt Dê", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("8Hải Sản", R.drawable.beefsteak));
+        food_sps.add(new Food_sp("9Sản phẩm khác", R.drawable.beefsteak));
         setFoodRecycler(food_sps);
 
         List<banner> banners = new ArrayList<>();
@@ -51,19 +51,22 @@ public class MainActivity extends AppCompatActivity implements FoodAdapter.OnFoo
         setBannerRecycler(banners);
 
         List<sale> sales = new ArrayList<>();
-        sales.add(new sale("ĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
-        sales.add(new sale("ĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
-        sales.add(new sale("ĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
-        sales.add(new sale("ĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
-        sales.add(new sale("ĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
-        sales.add(new sale("ĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
+        sales.add(new sale("aĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
+        sales.add(new sale("bĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
+        sales.add(new sale("cĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
+        sales.add(new sale("dĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
+        sales.add(new sale("eĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
+        sales.add(new sale("fĐÙI CỪU CÓ XƯƠNG CHUMP ON (XX:ÚC)", "₫280,000.00","₫250,000.00",R.drawable.mini_beef));
         setSaleRecycler(sales);
 
         List<bestseller> bestsellers = new ArrayList<>();
-        bestsellers.add(new bestseller("Thịt bò Mỹ-Canada-Nga", R.drawable.beef1,"đ100 000"));
-        bestsellers.add(new bestseller("Thịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
-        bestsellers.add(new bestseller("Thịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
-        bestsellers.add(new bestseller("Thịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
+        bestsellers.add(new bestseller("aThịt bò Mỹ-Canada-Nga", R.drawable.beef1,"đ100 000"));
+        bestsellers.add(new bestseller("bThịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
+        bestsellers.add(new bestseller("cThịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
+        bestsellers.add(new bestseller("dThịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
+        bestsellers.add(new bestseller("eThịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
+        bestsellers.add(new bestseller("fThịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
+        bestsellers.add(new bestseller("gThịt bò Mỹ-Canada-Nga", R.drawable.beefsteak,"đ100 000"));
         setBsRecycler(bestsellers);
 
     }
