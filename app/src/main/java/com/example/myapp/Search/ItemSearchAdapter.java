b19ce30a2a2b3f1bdf26b03f7ff8f0c2d4a62f79
Alars99 <hoanglong291099@gmail.com>
2020-08-27 18:12:15 +0700

    tai_sao_m_dung_vao_ItemSearch_cua_taooooooooooooo

--- app/src/main/java/com/example/myapp/Search/ItemSearchAdapter.java
+++ app/src/main/java/com/example/myapp/Search/ItemSearchAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,41 +14,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapp.Food_sp;
 import com.example.myapp.R;
+import com.example.myapp.bestseller;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
-public class ItemSearchAdapter extends RecyclerView.Adapter<ItemSearchAdapter.ItemSearchViewHolder>{
-
+public class ItemSearchAdapter extends RecyclerView.Adapter<ItemSearchAdapter.ItemSearchViewHolder> implements Filterable {
     Context context;
-    List<ItemSearch> itemSearchList;
+    ArrayList<ItemSearch> itemSearchList, filterList;
+    FilterProduct filter;
 
-    public ItemSearchAdapter(Context context, List<ItemSearch> itemSearchList) {
+    public ItemSearchAdapter(Context context, ArrayList<ItemSearch> itemSearchList){
         this.context = context;
         this.itemSearchList = itemSearchList;
+        this.filterList = itemSearchList;
     }
-
     @NonNull
     @Override
     public ItemSearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(context).inflate(R.layout.search_item, parent, false);
-
+        View view = LayoutInflater.from(context).inflate(R.layout.search_item,parent,false);
         return new ItemSearchViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemSearchViewHolder holder, int position) {
-//        holder.productImage.setImageResource(itemSearchList.get(position).getImg());
-//        holder.name.setText(itemSearchList.get(position).getName());
-
-
         ItemSearch itemSearch = itemSearchList.get(position);
-        String name = itemSearch.getName();
-        String Img = itemSearch.getImg();
+        holder.name.setText(itemSearchList.get(position).Name);
 
-        holder.name.setText(name);
+        String Img =  itemSearch.getImg();
 
         try
         {
@@ -56,9 +54,6 @@ public class ItemSearchAdapter extends RecyclerView.Adapter<ItemSearchAdapter.It
         {
             holder.productImage.setImageResource(R.drawable.beef1);
         }
-
-
-
     }
 
     @Override
@@ -66,7 +61,16 @@ public class ItemSearchAdapter extends RecyclerView.Adapter<ItemSearchAdapter.It
         return itemSearchList.size();
     }
 
-    public static final class ItemSearchViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public Filter getFilter() {
+        if(filter==null)
+        {
+            filter = new FilterProduct(this, filterList);
+        }
+        return filter;
+    }
+
+    public class ItemSearchViewHolder extends RecyclerView.ViewHolder {
 
         ImageView productImage;
         TextView name;
