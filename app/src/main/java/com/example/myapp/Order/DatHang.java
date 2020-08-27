c0565e2034f6f72033f58e96a5fe27f2c8de6f23
NTTTien <nguyentien1651999@gmail.com>
2020-08-27 18:18:16 +0700

    location

--- app/src/main/java/com/example/myapp/Order/DatHang.java
+++ app/src/main/java/com/example/myapp/Order/DatHang.java
@@ -1,28 +1,54 @@
 package com.example.myapp.Order;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import com.example.myapp.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class DatHang extends AppCompatActivity {
+public class DatHang extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     RecyclerView foodRecycler;
     FoodOrderAdapter FoodOrderAdapter;
+    private Location location;
+    private GoogleApiClient gac;
+    private TextView tvLocation;
+    Geocoder geocoder;
+    List<Address> addresses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dat_hang);
+        tvLocation = (TextView) findViewById(R.id.tvLocation);
+        if (checkPlayServices()) {
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+        }
     }
 
     private void setFoodRecycler(List<FoodOrder> foodorder_spList)
@@ -42,5 +68,86 @@ public class DatHang extends AppCompatActivity {
     }
     public void btnBack(View view) {
         super.onBackPressed();
+    }
+
+    public void dispLocation(View view) {
+        getLocation();
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Kiểm tra quyền hạn
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+        } else {
+            location = LocationServices.FusedLocationApi.getLastLocation(gac);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                geocoder = new Geocoder(this, Locale.getDefault());
+                try{
+
+                    addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                    String address = addresses.get(0).getAddressLine(0);
+                    String area = addresses.get(0).getLocality();
+                    String city= addresses.get(0).getAdminArea();
+                    String country = addresses.get(0).getCountryName();
+
+                    String fulladress = address +", "+area +", "+city+", "+country+",";
+                    tvLocation.setText(fulladress);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                tvLocation.setText("(Không thể hiển thị vị trí. " +"Bạn đã kích hoạt location trên thiết bị chưa?)");
+            }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        if (gac == null) {
+            gac = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API).build();
+        }
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, 1000).show();
+            } else {
+                Toast.makeText(this, "Thiết bị này không hỗ trợ.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        getLocation();
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        gac.connect();
+    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Lỗi kết nối: " + connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+    protected void onStart() {
+        gac.connect();
+        super.onStart();
+    }
+    protected void onStop() {
+        gac.disconnect();
+        super.onStop();
     }
 }
